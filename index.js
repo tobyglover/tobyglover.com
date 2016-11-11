a7c7837753c278336be90db23ca0befccec4bd36
Toby Glover <william.glover@tufts.edu>
2016-11-11 16:25:55 -0500

    ported static website into node

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+ 	console.log('Node app is running on port', app.get('port'));
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use("/styles",express.static(__dirname + "/views/stylesheets"));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.render('pages/index', {
+		title: "Toby Glover",
+		navLinks: getNavLinks('Home')
+	});
+});
+
+app.get('/resume', function(request, response) {
+	response.render('pages/resume', {
+		title: "Resume",
+		navLinks: getNavLinks('Resume')
+	});
+});
+
+function getNavLinks(pageName) {
+	var navLinks = {Home: "/",
+					Resume: "/resume",
+					Projects: "/resume"};
+	delete navLinks[pageName];
+	return navLinks;
+}
+
+function enableCORS(response) {
+	response.header("Access-Control-Allow-Origin", "*");
+  	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+}
