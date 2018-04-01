3fca3184956fc495e91698e761f7e22af7806e9e
Toby Glover <william.glover@tufts.edu>
2018-04-01 14:53:59 -0400

    minor wording changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express    = require('express');
 var app        = express();
 var apiRouter  = require('./src/server/routers/api-router');
 var pageRouter = require('./src/server/routers/page-router');
-var opn = require('opn');
+var opn        = require('opn');
 
 global.test   = false;
 var localPort = 5000;
